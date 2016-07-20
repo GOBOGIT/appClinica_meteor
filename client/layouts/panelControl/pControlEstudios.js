3f7a35c0a001dec559ee705a49db2018c8f32908
Carlos Santos Mateo // GOBO.TV <carlos@gobo.tv>
2016-07-20 11:28:06 -0500

    Modificar valores en Mongo

--- client/layouts/panelControl/pControlEstudios.js
+++ client/layouts/panelControl/pControlEstudios.js
@@ -4,16 +4,27 @@ import './pControlEstudios.html';
 
 state = new ReactiveDict();
 
+var sel;
+
 Template.listaPC.onCreated(function () {
-    state.set('btPromo', false);
+ //   state.set('btPromo', false);
     state.set('pantallaInicio', true);
 
     Meteor.subscribe('itemEstudios');
 });
 
 Template.detallesPC.helpers({
+    selPromo() {
+        return state.get('esPromo');
+    },
+
     estudios() {
-        return Estudios.findOne({ 'titulo': state.get('seleccion') });
+         // limpia la validación del formulario   
+        resetForm();
+        // recoge datos de la colleción según selección
+       sel = Estudios.findOne({ 'titulo': state.get('seleccion') });
+        state.set('esPromo', sel.esPromo);
+        return sel;
     }
 });
 
@@ -32,15 +43,89 @@ Template.listaPC.events({
     }
 });
 
-
-/*
-Template.detallesPC.events({
-    'click .sliderPromo'(event) {
+Template.panelControlPpalEstudios.events({
+    // 'submit #formularioEstudios'(event){
+        
+    'submit #formularioEstudios'(event){
+        
+      //  console.log(state.get('esPromo'));
+        Estudios.update(sel._id, {
+            $set: { 
+            'titulo':  event.target.titulo.value
+      },
+        });
         event.preventDefault();
-
-        console.log("entra");
-
+        state.set('pantallaInicio', false);
+        state.set('seleccion',event.target.titulo.value) ;
     }
 });
-*/
 
+
+// activa o desactiva las opciones de promoción
+Template.detallesPC.events({
+    'submit .sliderPromo'(event) {
+        event.preventDefault();
+        
+        state.set('esPromo', !state.get('esPromo'));
+    }
+});
+
+function resetForm() {
+  var form = $('#formularioEstudios');
+  form.form('reset');
+  form.form('clear');
+  // remove the error class so it wont show error msg ( semantic-ui bug ? )
+  form.removeClass('error');
+}
+
+//********** VALIDACION
+
+Template.detallesPC.onRendered(function () {
+    
+    $('.ui.form')
+        .form({
+            fields: {
+                titulo: {
+                    identifier: 'titulo',
+                    rules: [{
+                        type: "empty"
+                    }]
+                },
+                validoHasta: {
+                    identifier: 'validoHasta',
+                    optional: true,
+                    rules: [{
+                        type: "regExp[/^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\\d\\d$/]"
+                    }]
+                },
+                validoDesde: {
+                    identifier: 'validoDesde',
+                    optional: true,
+                    rules: [{
+                        type: "regExp[/^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\\d\\d$/]"
+                    }]
+                },
+                promoValidoHasta: {
+                    identifier: 'promoValidoHasta',
+                    optional: true,
+                    rules: [{
+                        type: "regExp[/^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\\d\\d$/]"
+                    }]
+                },
+                precioPromo: {
+                    identifier: 'precioPromo',
+                    optional: true,
+                    rules: [{
+                        type: "number"
+                    }]
+                },
+                precio: {
+                    identifier: 'precio',
+                    optional: true,
+                    rules: [{
+                        type: "number"
+                    }]
+                },
+            }
+        });
+});
